c6d6883bd5ba6f9e966c0313d6fb4253f5d73dae
ada0104 <48831124+ada0104@users.noreply.github.com>
2020-02-11 17:37:41 +0800

    0211

--- insert.php
+++ insert.php
@@ -1,67 +1,115 @@
 <?php
-// require_once "db.php";
-// header('Access-Control-Allow-Origin:*');
-//echo '123';
-$option='';
-$name ='';
-$context='';
 
-$option = $_POST['option'] ? $_POST['option'] : '';
+require_once 'db.php';
+
+$method = $_SERVER['REQUEST_METHOD'];
+
+if (!isset($method)){
+    return ['未帶入參數'];
+};
+
+  switch ($_SERVER['REQUEST_METHOD'])
+  {
+    case 'POST':
+        $option = $_POST['option'] ? $_POST['option'] : '';
+      break;
+    case 'GET':
+        $option = $_GET['option'] ? $_GET['option'] : '';
+      break;
+    case 'DELETE':
+        $option = 'delete';
+      break;
+    case 'PUT':
+        parse_str(file_get_contents('php://input'), $_PUT);
+        $option = $_PUT['option'];
+      break;
+  };
 
 
-switch($option){
+if($option === 'insert'){
+    $name = $_POST['name'];
+    $context = $_POST['context'];
+    $name = htmlspecialchars(preg_replace("/[\'\"]+/" , '', $name));
+    $context = htmlspecialchars(preg_replace("/[\'\"]+/" , '', $context));
+    echo json_encode(insert($name, $context));
+}elseif($option === 'delete'){
+    $id = $_SERVER['QUERY_STRING'];
+    $id = htmlspecialchars(preg_replace('/[^\d]/', '',$id));
+    echo json_encode(delete($id));
+}elseif($option === 'edit'){
+    $updateId = $_PUT['updateId'];;
+    $updateName = $_PUT['updateName'];;
+    $updateContext = $_PUT['updateContext'];;
+    $updateId = htmlspecialchars(preg_replace("/[\'\"]+/" , '', $updateId));
+    $updateName =  htmlspecialchars(preg_replace("/[\'\"]+/" , '', $updateName));
+    $updateContext =  htmlspecialchars(preg_replace("/[\'\"]+/" , '', $updateContext));
+    echo json_encode(edit($updateId, $updateName, $updateContext));
+}elseif($option === 'view'){
+    echo json_encode(view());
+};
 
-    case 'insert':
-        $name = $_POST['name'];
-        $context = $_POST['context'];
-        echo json_encode(insert($name, $context));
-        break;
-    case 'delete':
-        $id = $_POST['id'];
-        echo json_encode(delete($id));
-        break;
-    case 'edit':
-        $updateId = $_POST['updateId'];
-        $updateName = $_POST['updateName'];
-        $updateContext = $_POST['updateContext'];
-        echo json_encode(edit($updateId, $updateName, $updateContext));
-        break;
-    case 'view':
-        echo json_encode(view());
-        break;
-
-}
 function view(){
-    require_once "db.php";
-    $sql = "SELECT `id`,`name`,`context` FROM `context`ORDER BY `context`.`id` ASC";
+    global $conn;
+    $sql = 'SELECT id,name,context FROM context ORDER BY context.id ASC';
     $res = $conn->query($sql)or die($conn->connect_error);
     $data = mysqli_fetch_all($res, MYSQLI_ASSOC);
     $conn->close();
     return json_encode($data);
 }
 function insert($name,$context) {
-    require_once "db.php";
-    $sql = "INSERT INTO `context` (`id`, `name`, `context`, `other`) VALUES (NULL,'$name','$context','')";
+    global $conn;
+    $sql = "INSERT INTO context (id, name, context, other) VALUES (NULL,'$name','$context','')";
     $res = $conn->query($sql);
     $conn->close();
-    $data = ['新增成功'];
-    return $data;
+    return ['新增成功'];
 }
 function delete($id) {
-    require_once "db.php";
-    $sql = " DELETE FROM `context` WHERE `context`.`id` = '$id'";
+    global $conn;
+    $sql ="SELECT EXISTS(SELECT * FROM context WHERE id = '$id')";
     $res = $conn->query($sql);
-    $conn->close();
-    $data = ['刪除成功'];
-    return $data;
+    if($res){
+        $sql = "DELETE FROM context WHERE context.id = '$id'";
+        $res = $conn->query($sql);
+        $conn->close();
+        return ['刪除成功'];
+    }else{
+        return ['無此筆資料'];
+    }
 }
 function edit($updateId, $updateName, $updateContext) {
-    require_once "db.php";
-    $sql = " UPDATE `context` SET `name` = '$updateName', `context` = '$updateContext' WHERE `context`.`id` = '$updateId';";
+    global $conn;
+    $sql ="SELECT EXISTS(SELECT * FROM context WHERE id = '$updateId')";
     $res = $conn->query($sql);
-    $conn->close();
-    $data = ['修改成功'];
-    return $data;
-}
+    if($res){
+        $sql = "UPDATE context SET name = '$updateName', context = '$updateContext' WHERE context.id = '$updateId'";
+        $res = $conn->query($sql);
+        $conn->close();
+        return  ['修改成功'];
+    }else{
+        return ['無此筆資料'];
+    }
 
+}
+// $returnData = $dbh->prepare("SELECT
+// user_info.user_id,
+// user_info.uname,
+// organization.name,
+// user_info.grade,
+// user_info.class
+// FROM
+// user_family
+// LEFT JOIN
+// user_info ON user_family.user_id = user_info.user_id
+// LEFT JOIN
+// organization ON user_info.organization_id = organization.organization_id
+// WHERE
+// fuser_id = :fuser_id AND user_info.user_id IS NOT NULL
+// ");
+
+
+// $returnData->bindValue(':fuser_id', $_POST['fuser_id'], PDO::PARAM_STR);
+// $returnData->execute();
+// $returnData =$returnData->fetchAll(\PDO::FETCH_ASSOC);
+
+// echo json_encode($returnData, JSON_UNESCAPED_UNICODE);
 ?>
--- messageBoard.sql
+++ messageBoard.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- 主機： localhost
--- 產生時間： 2020 年 02 月 10 日 07:35
+-- 產生時間： 2020 年 02 月 10 日 09:05
 -- 伺服器版本： 5.7.29
 -- PHP 版本： 7.2.27
 
@@ -21,6 +21,8 @@ SET time_zone = "+00:00";
 --
 -- 資料庫： `messageBoard`
 --
+CREATE DATABASE IF NOT EXISTS `messageBoard` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `messageBoard`;
 
 -- --------------------------------------------------------
 
@@ -51,7 +53,9 @@ INSERT INTO `context` (`id`, `name`, `context`, `other`) VALUES
 (16, 'aaa', 'cca', ''),
 (17, 'AAA', 'AAA', ''),
 (19, '我是ada', '1234', ''),
-(20, 'aaa', 'aaaaaa', '');
+(20, 'aaa', 'aaaaaa', ''),
+(21, '123', '123', ''),
+(22, 'aaaa', '123', '');
 
 --
 -- 已傾印資料表的索引
@@ -71,7 +75,7 @@ ALTER TABLE `context`
 -- 使用資料表自動遞增(AUTO_INCREMENT) `context`
 --
 ALTER TABLE `context`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
